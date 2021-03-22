4b6f72f8fa088e77390060516488ed6f7a34cb53
Tangjj1996 <tangjj_1996@163.com>
2021-03-22 16:34:08 +0800

    feat: tsx config

---
+++ src/app/homepage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+interface Props {
+  name: String
+}
+class HelloComponent extends React.Component {
+  constructor(props: Props) {
+    super(props)
+  }
+  render() {
+    return (
+     <div>hahahah</div>
+    )
+  }
+}
+
+export default HelloComponent
